9936fd68c51f59638dd7f0d9922f42866d7f40a4
mo086 <mo086@users.noreply.github.com>
2024-06-10 10:31:59 +0000

    deploy: c608c6267d600304751882cedfdda1511f37d4cc

--- assets/index-AR50tz09.js
+++
@@ -1,1 +0,0 @@
-import{d as e,u as n}from"./app-Ck39Kc3H.js";const i=e({name:"IndexPage",__name:"index",setup(o){return n().push(window.innerHeight>window.innerWidth?"/m":"/w"),()=>{}}});export{i as default};
---
+++ assets/index-BVFYymUv.js
@@ -0,0 +1,1 @@
+import{d as e,i as n,u as i}from"./app-BEsfPXC0.js";const s=e({name:"IndexPage",__name:"index",setup(t){return n&&i().push(window.innerHeight>window.innerWidth?"/m":"/w"),()=>{}}});export{s as default};
