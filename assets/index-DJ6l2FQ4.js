1b01608fbc7b2efa361903c91a53e35ee8f0a4e3
mo086 <mo086@users.noreply.github.com>
2024-06-10 01:40:08 +0000

    deploy: 6b5df09b447a3075c3dcd65c1cf4b29f237f3af2

---
+++ assets/index-DJ6l2FQ4.js
@@ -0,0 +1,1 @@
+import{d as n,o,c as t}from"./app-UcltYj0e.js";const c=n({name:"IndexPage",__name:"index",setup(a){return(r,s)=>(o(),t("div"))}}),e={};typeof e=="function"&&e(c);export{c as default};
